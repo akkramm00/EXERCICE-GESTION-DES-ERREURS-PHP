d991c37418d43199518c3b6c80730a02aa5012ba
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-27 12:51:09 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+ <html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php 
+// Vous etes développeur et vous devez écrire un programme PHP pour l'entreprise Strategy,
+//en vue d'améliorer un processur de publication vers le web. Lentrepprise strategy est dans le domaine de la veille stratégique.
+// Elle recense et traite quotidiennement des centaines de publications dans différents domaines  : academique, juridique, commercial, etc.
+// Depuis peu , elle récup-re des résumés (astracts) de publications au format Markdown. Elle aimerait pouvoir les transformer  automatiquement
+// au format HTML pour les publier sur un site web.
+
+?> 
+
+
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+  </body>
+</html>
